4d8b909b12c0d6fb1ffaa45c670118806b6a94c8
Xyuan <906162099@qq.com>
2025-06-23 12:24:05 +0800

    feat:gitignore 修改

---
+++ lang/英文.js
@@ -0,0 +1,16 @@
+export default {
+  "skin": {
+    "Blue": "\r",
+    "Green": "\r",
+    "Red": "\r",
+    "Purple": "\r",
+    "Default": "\r"
+  },
+  "layout": {
+    "nav": {
+      "title": "\r",
+      "languageSwitch": "\r",
+      "theme": ""
+    }
+  }
+}
